7355bffd9553cf4d553c2eee2df8fa44b415cbe2
w15706853015 <lyxp972672845@163.com>
2022-09-21 10:48:47 +0800

    每个框添加内边距

--- src/views/taskFlowPlan/navigation.vue
+++ src/views/taskFlowPlan/navigation.vue
@@ -20,6 +20,7 @@
 
 <script>
 export default {
+  name: "Navigation",
   data() {
     return {
       queryParams: {
--- src/views/taskFlowPlan/processManagement.vue
+++ src/views/taskFlowPlan/processManagement.vue
@@ -1,17 +1,28 @@
 <template>
   <div class="pro-box">
     <el-row :gutter="20">
-      <el-col :span="10">
+      <el-col :span="7">
         匹配任务:
       </el-col>
-      <el-col :span="14"></el-col>
+      <el-col :span="14">
+        <div class="btn">{{process.num1}}</div>
+      </el-col>
     </el-row>
   </div>
 </template>
 
 <script>
 export default {
-
+  name: "ProcessManagement",
+  data() {
+    return {
+      process: {
+        num1: 5678,
+        num2: 5678,
+        num3: 5678,
+      }
+    }
+  }
 }
 </script>
 
