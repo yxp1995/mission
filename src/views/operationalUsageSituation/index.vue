0919fad352a29d0620352bf50fc679eabc1cda36
weizhiyong <weizhiy12345678@163.com>
2022-09-21 13:43:51 +0800

    图形

--- src/views/operationalUsageSituation/index.vue
+++ src/views/operationalUsageSituation/index.vue
@@ -1,18 +1,115 @@
 <template>
-    <div>
-        作战使用态势
+  <div class="datacommon-container">
+    <div class="main">
+      <div class="main-left">
+        <div class="left-top">
+          <div class="text-title">
+            <div class="text-img">
+              <img src="~@/assets/images/eight.png" alt="" />
+              <img src="~@/assets/images/seven.png" alt="" />
+            </div>
+            <div class="text-txt">导航窗口</div>
+            <div class="text-img">
+              <img src="~@/assets/images/seven.png" alt="" />
+              <img src="~@/assets/images/nine.png" alt="" />
+            </div>
+          </div>
+          <div class="main-container">
+            <navigation />
+          </div>
+        </div>
+        <div class="left-btm">
+          <div class="text-title">
+            <div class="text-img">
+              <img src="~@/assets/images/eight.png" alt="" />
+              <img src="~@/assets/images/seven.png" alt="" />
+            </div>
+            <div class="text-txt">任务编辑窗口</div>
+            <div class="text-img">
+              <img src="~@/assets/images/seven.png" alt="" />
+              <img src="~@/assets/images/nine.png" alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="main-center">
+        <div class="center-top">
+          <div class="text-title">
+            <div class="text-img">
+              <img src="~@/assets/images/eight.png" alt="" />
+              <img src="~@/assets/images/seven.png" alt="" />
+            </div>
+            <div class="text-txt">任务总览图</div>
+            <div class="text-img">
+              <img src="~@/assets/images/seven.png" alt="" />
+              <img src="~@/assets/images/nine.png" alt="" />
+            </div>
+          </div>
+        </div>
+        <div class="center-btm">
+          <div class="text-title">
+            <div class="text-img">
+              <img src="~@/assets/images/eight.png" alt="" />
+              <img src="~@/assets/images/seven.png" alt="" />
+            </div>
+            <div class="text-txt">产品选择</div>
+            <div class="text-img">
+              <img src="~@/assets/images/seven.png" alt="" />
+              <img src="~@/assets/images/nine.png" alt="" />
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="main-right">
+        <div class="right-top">
+          <div class="text-title">
+            <div class="text-img">
+              <img src="~@/assets/images/eight.png" alt="" />
+              <img src="~@/assets/images/seven.png" alt="" />
+            </div>
+            <div class="text-txt">使用分布</div>
+            <div class="text-img">
+              <img src="~@/assets/images/seven.png" alt="" />
+              <img src="~@/assets/images/nine.png" alt="" />
+            </div>
+          </div>
+          <using-distribution />
+        </div>
+        <div class="right-btm">
+          <div class="text-title">
+            <div class="text-img">
+              <img src="~@/assets/images/eight.png" alt="" />
+              <img src="~@/assets/images/seven.png" alt="" />
+            </div>
+            <div class="text-txt">提示窗口</div>
+            <div class="text-img">
+              <img src="~@/assets/images/seven.png" alt="" />
+              <img src="~@/assets/images/nine.png" alt="" />
+            </div>
+          </div>
+          <prompt/>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
+import navigation from "../taskFlowPlan/navigation.vue";
+import usingDistribution from "./usingDistribution.vue";
+import prompt from "./prompt.vue"
 export default {
- 
-   data(){
-    return {
-
-    }
-   }
-}
+  name: "OperationalUsageSituation",
+  components: {
+    navigation,
+    usingDistribution,
+    prompt
+  },
+  data() {
+    return {};
+  },
+};
 </script>
 
-<style scoped></style>
+<style lang="scss" scoped>
+</style>
---
+++ src/views/operationalUsageSituation/prompt.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <el-table :data="List" stripe style="width:95%;margin-left:7px" height="100%"  align="center" size="mini" :row-style="tableRowStyle" :header-row-style="headerStyle">
+      <el-table-column prop="content" label="提示内容" align="center"> </el-table-column>
+      <el-table-column prop="date" label="时间" align="center"> </el-table-column>
+      <el-table-column prop="other" label="待补充" align="center"> </el-table-column>
+    </el-table>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      List: [
+        { id: 1, content: "内容1", date: "11/21 16:45:45", other: "11.1" },
+        { id: 2, content: "内容2", date: "11/21 16:45:45", other: "11.1" },
+        { id: 3, content: "内容3", date: "11/21 16:45:45", other: "11.1" },
+      
+        
+      ],
+    };
+  },
+  methods:{
+    tableRowStyle(){
+        return{
+            color:"red",
+           
+        }
+    },
+    headerStyle(){
+        return{
+            color:"#409EFF",
+     
+            
+        }
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+ 
+</style>
